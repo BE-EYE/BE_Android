f1b01ae62a9ea381149735bea5349583756453f4
915dbfl <dbfl00915@naver.com>
2023-02-16 21:17:29 +0900

    [refactor] #2: 패키지 setting

--- app/src/main/java/com/example/beeye/ui/MainActivity.kt
+++ app/src/main/java/com/example/beeye/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.beeye
+package com.example.beeye.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.beeye.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
