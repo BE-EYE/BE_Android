6df747d0dafe35e764f19bbf1a4088a2df480cd3
915dbfl <dbfl00915@naver.com>
2023-02-16 23:47:51 +0900

    [feat] #5: navigation 적용

--- app/src/main/java/com/example/beeye/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/beeye/ui/view/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.beeye.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.beeye.R
 import com.example.beeye.databinding.ActivityMainBinding
 
@@ -10,13 +15,26 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupJetpackNavigation()
+    }
 
-        if (savedInstanceState == null) {
-            binding.bottomNavigationView.selectedItemId = R.id.fragment_home
-        }
+    private fun setupJetpackNavigation() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment ?: return
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.fragment_home, R.id.fragment_archive
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
