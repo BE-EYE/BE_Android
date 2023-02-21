58a40a83622760376f1835e2af1bfbf0fe4eb519
915dbfl <dbfl00915@naver.com>
2023-02-21 16:24:05 +0900

    [feat] #18: homeFragment 카메라, 이미지 버튼에 ocr 기능 추가

--- app/src/main/java/com/example/beeye/ui/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/beeye/ui/view/home/HomeFragment.kt
@@ -12,24 +12,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
 import com.example.beeye.databinding.FragmentHomeBinding
-import java.io.IOException
+import com.example.beeye.ui.viewmodel.HomeViewModel
+import java.io.*
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding get() = _binding!!
+    private lateinit var homeViewModel: HomeViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        homeViewModel.setFragmentActivity(requireActivity())
+        homeViewModel.checkFile("kor")
+        homeViewModel.checkFile("eng")
 
         binding.btnMainCamera.setOnClickListener {
             requestCameraLauncher.launch(REQUIRED_CAMERA_PERMISSION)
@@ -65,8 +73,8 @@ class HomeFragment : Fragment() {
         ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == RESULT_OK) {
             //deprecated 처리 필요
-            val bitmap = it.data?.extras?.get("data")
-            Log.d("bitmap", bitmap.toString())
+            val bitmap = it.data?.extras?.get("data") as Bitmap
+            homeViewModel.getOcrText(bitmap)
             //요약 페이지로 전환 필요
         }
     }
@@ -77,12 +85,16 @@ class HomeFragment : Fragment() {
                 try {
                     val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                         val source = ImageDecoder.createSource(requireActivity().contentResolver, uri!!)
-                        ImageDecoder.decodeBitmap(source)
+                        val src = ImageDecoder.decodeBitmap(source)
+                        val bitmap = src.copy(Bitmap.Config.ARGB_8888, false)
+
+                        homeViewModel.getOcrText(bitmap)
+
                     } else {
                         MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, uri)
                     }
 
-                    //bitmap 처리
+                    //navigation으로 데이터를 요약 페이지로 넘겨줌
                     Log.d("bitmap", bitmap.toString())
                 }catch (e: IOException) {
                     Log.e("버전 에러", e.toString())
